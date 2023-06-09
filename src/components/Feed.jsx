7a1d7a378b2b61b27234830be58f15cd1b1efc32
Austin Friesorger <aafrieso@svsu.edu>
2023-06-09 17:40:38 -0400

    add videos component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import Sidebar from './Sidebar';
+import { Sidebar, Videos } from './';
 
 const Feed = () => {
   return (
@@ -12,6 +12,15 @@ const Feed = () => {
         variant="body2" sx={{ mt: 1.5, color: '#fff'}}>
           copyright 2023 Google LLC
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2}}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white"}}>
+          New <span style={{ color: 'F31503'}}>videos</span>
+        </Typography>
+
+        <Videos videos={[]} />
+
       </Box>
     </Stack>
   )
