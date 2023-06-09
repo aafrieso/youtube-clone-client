f7c3749412245a275127d7d1ce98fc0adff665d6
Austin Friesorger <aafrieso@svsu.edu>
2023-06-09 00:14:01 -0400

    add sidebar icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Stack } from '@mui/material';
+import { categories } from '../utils/constants';
 
-const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+const Sidebar = () => (
+  <Stack direction="row" sx={{overflow: 'auto', height: { sx: 'auto',md: '95%' }, flexDirection: { md: 'column'}}}
+  >
+    {categories.map((category) => (
+      <button>
+        <span>{category.icon}</span>
+        <span>{category.name}</span>
+
+      </button>
+    ))}
+
+
+  </Stack>
+)
 
 export default Sidebar
