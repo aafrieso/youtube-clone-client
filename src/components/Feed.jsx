9eecfd32b261646ca0ed31f8beb5eaf6310848fb
Austin Friesorger <aafrieso@svsu.edu>
2023-06-09 18:08:17 -0400

    update use effect for api

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,15 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
+
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 import { Sidebar, Videos } from './';
 
 const Feed = () => {
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+  }, []);
+
   return (
     <Stack sx={{ flexDirection: { sx: 'column', md: 'row' }}}>
       <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 }}}>
