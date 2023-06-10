f05f0b1e0a5176067505d92f1c16bf0c2c20ddf7
Austin Friesorger <aafrieso@svsu.edu>
2023-06-10 14:22:01 -0400

    add videos and channel card components

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { Stack, Box } from '@mui/material';
 
 const Videos = ({ videos }) => {
   return (
-    <div>Videos</div>
+    <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
+      {videos.map((items, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+
+        </Box>
+      ))}
+    </Stack>
   )
 }
 
